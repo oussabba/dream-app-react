8a4fc1aa2dd6ddbaf64bb488a86425aa05528a4f
Oussama Babile <o.babile@devotech.co>
2023-08-21 15:18:20 +0100

    fix: button margin

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,7 @@ function Button({ onClick }) {
   return (
     <div
       onClick={onClick}
-      className="bg-[#80B1B7] rounded-lg w-[150px] h-[35px] mx-auto mt-[30px] text-white text-[11px] font-bold flex items-center justify-center drop-shadow-lg cursor-pointer"
+      className="bg-[#80B1B7] rounded-lg w-[150px] h-[35px] mx-auto my-[30px] text-white text-[11px] font-bold flex items-center justify-center drop-shadow-lg cursor-pointer"
     >
       EXPRIMER MES RÊVES&gt;
     </div>
